5fe2ff7467e437b84ee8ec6e09fe2c3377648562
Cale Crawford <calecrawford@icloud.com>
2019-02-28 11:41:57 -0800

    Update router.rb

--- lib/open_api/router.rb
+++ lib/open_api/router.rb
@@ -15,7 +15,7 @@ module OpenApi
             all_routes = Rails.application.routes.routes
             require 'action_dispatch/routing/inspector'
             inspector = ActionDispatch::Routing::RoutesInspector.new(all_routes)
-            inspector.format(ActionDispatch::Routing::ConsoleFormatter::Expanded.new, {})
+            inspector.format(ActionDispatch::Routing::ConsoleFormatter::Sheet.new, {})
             # :nocov:
           end
     end
